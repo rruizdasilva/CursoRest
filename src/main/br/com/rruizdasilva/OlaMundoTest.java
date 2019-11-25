9cf372b690f7555dc210222f1b82e0fb5ff587c7
Roger <roger.dasilva@ul.com>
2019-11-25 09:53:57 -0300

    Junit class

---
+++ src/main/br/com/rruizdasilva/OlaMundoTest.java
@@ -0,0 +1,21 @@
+package br.com.rruizdasilva;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OlaMundoTest {
+
+    @Test
+    public void testOlaMundo() {
+        Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+        Assert.assertEquals("Ola Mundo!", response.getBody().asString());
+        Assert.assertEquals(200, response.statusCode());
+
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
+    }
+ }

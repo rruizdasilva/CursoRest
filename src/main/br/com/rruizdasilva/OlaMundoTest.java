a5e3a0cc041f18e90883f8e7f57265e3bb925a24
Roger <roger.dasilva@ul.com>
2019-11-27 08:23:52 -0300

    Json - Second level

---
+++ src/main/br/com/rruizdasilva/OlaMundoTest.java
@@ -0,0 +1,80 @@
+package br.com.rruizdasilva;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class OlaMundoTest {
+
+    @Test
+    public void testOlaMundo() {
+        Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+        Assert.assertEquals("Ola Mundo!", response.getBody().asString());
+        Assert.assertEquals(200, response.statusCode());
+
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
+    }
+
+    @Test
+    public void devoConhecerOutrasFormasRestAssured() {
+        get("http://restapi.wcaquino.me/ola").then().statusCode(200);
+    }
+
+    @Test
+    public void formaFluente() {
+        given() //Pré condições
+        .when() // Ação
+        .get("http://restapi.wcaquino.me/ola")
+        .then() // Assertivas
+//      .assertThat()
+        .statusCode(200);
+    }
+
+    @Test
+    public void devoConhecerMatchersHamcrest(){
+        Assert.assertThat("Maria", Matchers.is("Maria"));
+        Assert.assertThat(128, Matchers.is(128));
+        Assert.assertThat(128, Matchers.isA(Integer.class));
+        Assert.assertThat(128d, Matchers.isA(Double.class));
+        Assert.assertThat(128d, Matchers.greaterThan(120d));
+        Assert.assertThat(128d, Matchers.lessThan(130d));
+
+        List<Integer> impares = Arrays.asList(1, 3, 5, 7, 9);
+        assertThat(impares, hasSize(5));
+        assertThat(impares, contains(1, 3, 5, 7, 9));
+        assertThat(impares, containsInAnyOrder(1, 3, 5, 9, 7));
+        assertThat(impares, hasItem(1));
+        assertThat(impares, hasItems(1, 5));
+
+        assertThat("Maria", is(not("João")));
+        assertThat("Maria", (not("João"))); // sem o 'is'
+        assertThat("Joaquina", anyOf(is("Maria"), is("Joaquina")));
+        assertThat("Joaquina", allOf(startsWith("Joa"), endsWith("ina"), containsString("qui")));
+    }
+
+    @Test
+    public void devoValidarBody() {
+        given() //Pré condições
+                .when()
+                .get("http://restapi.wcaquino.me/ola")
+                .then()
+                .statusCode(200) // começar com as validações que têm maior impacto pode tornar o código mais performático
+                // ordem > do mais para o menos restritivo
+                .body(is("Ola Mundo!"))
+                .body(containsString("Mundo"))
+                .body(is(notNullValue()));
+    }
+}

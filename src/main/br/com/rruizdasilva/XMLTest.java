7ad664c79278325cefef40a3e099a459128e55f7
Roger <roger.dasilva@ul.com>
2019-11-28 08:48:36 -0300

    Working with XML + Rootpath

--- src/main/br/com/rruizdasilva/XMLTest.java
+++ src/main/br/com/rruizdasilva/XMLTest.java
@@ -24,4 +24,28 @@ public class XMLTest {
                 .body("user.filhos.name", hasItems("Luizinho", "Zezinho"))
         ;
     }
+
+    @Test
+    public void deveTrabalharComXMLeRootPath(){
+        given()
+                .when()
+                .get("http://restapi.wcaquino.me/usersXML/3")
+                .then()
+                .statusCode(200)
+                .rootPath("user")
+                .body("name", is("Ana Julia"))
+                .body("@id", is("3"))
+
+                .rootPath("user.filhos")
+                .body("name.size()", is(2))
+
+                .detachRootPath("filhos")
+                .body("filhos.name[0]", is("Zezinho"))
+
+                .appendRootPath("filhos")
+                .body("name[1]", is("Luizinho"))
+                .body("name", hasItem("Luizinho"))
+                .body("name", hasItems("Luizinho", "Zezinho"))
+        ;
+    }
 }

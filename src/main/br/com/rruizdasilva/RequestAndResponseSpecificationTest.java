96e380e5b486d872fa650e8a85af7feab3330c87
Roger <roger.dasilva@ul.com>
2019-11-29 08:56:47 -0300

    Request/Response Specification

---
+++ src/main/br/com/rruizdasilva/RequestAndResponseSpecificationTest.java
@@ -0,0 +1,49 @@
+package br.com.rruizdasilva;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.internal.path.xml.NodeImpl;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.request;
+import static org.hamcrest.Matchers.*;
+
+public class RequestAndResponseSpecificationTest {
+    public static RequestSpecification requestSpec;
+    public static ResponseSpecification responseSpec;
+
+    @BeforeClass
+    public static void setup() {
+        RestAssured.baseURI = "https://restapi.wcaquino.me";
+        RestAssured.port = 443; // como este é o valor padrão, este comando pode ser omitido
+        RestAssured.basePath = ""; // como o valor é vazio, este comando pode ser omitido
+
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.log(LogDetail.ALL);
+        requestSpec = requestSpecBuilder.build();
+
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+        responseSpecBuilder.expectStatusCode(200);
+        responseSpec = responseSpecBuilder.build();
+    }
+
+    @Test
+    public void deveTrabalharComXML() {
+        given()
+                .when()
+                .get("/usersXML/3")
+                .then()
+                // .statusCode(200)
+                .spec(responseSpec)
+        ;
+    }
+}

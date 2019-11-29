fe250731d8327e0bfd1349094a3efaedb909b243
Roger <roger.dasilva@ul.com>
2019-11-29 08:26:27 -0300

    Advanced search with Xpath

--- src/main/br/com/rruizdasilva/XMLTest.java
+++ src/main/br/com/rruizdasilva/XMLTest.java
@@ -88,4 +88,28 @@ public class XMLTest {
         Assert.assertTrue("ANA JULIA".equalsIgnoreCase(nomes.get(1).toString()))
         ;
     }
+
+    @Test
+    public void devoFazerPesquisasAvancadasComXpath(){
+        given()
+                .when()
+                .get("http://restapi.wcaquino.me/usersXML")
+                .then()
+                .statusCode(200)
+                .body(hasXPath("count(/users/user)", is("3")))
+                .body(hasXPath("/users/user[@id = '1']"))
+                .body(hasXPath("//user[@id = '2']"))
+                .body(hasXPath("//name[text() = 'Luizinho']/../../name", is("Ana Julia")))
+                .body(hasXPath("//name[text() = 'Ana Julia']/following-sibling::filhos",
+                        allOf(containsString("Zezinho"), containsString("Luizinho"))))
+                .body(hasXPath("/users/user/name", is("João da Silva")))
+                .body(hasXPath("//name", is("João da Silva")))
+                .body(hasXPath("/users/user[2]/name", is("Maria Joaquina")))
+                .body(hasXPath("/users/user[last()]/name", is("Ana Julia")))
+                .body(hasXPath("count(/users/user/name[contains(. , 'n')])", is("2")))
+                .body(hasXPath("/users/user[age < 24]/name", is("Ana Julia")))
+                .body(hasXPath("/users/user[age > 20 and age < 30]/name", is("Maria Joaquina")))
+                .body(hasXPath("/users/user[age > 20][age < 30]/name", is("Maria Joaquina")))
+        ;
+    }
 }

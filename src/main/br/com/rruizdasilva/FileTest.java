38340afb80384ce5d08a03c6b69bea97893c2b98
Roger <roger.dasilva@ul.com>
2019-12-04 08:38:29 -0300

    File test

---
+++ src/main/br/com/rruizdasilva/FileTest.java
@@ -0,0 +1,23 @@
+package br.com.rruizdasilva;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class FileTest {
+
+    @Test
+    public void devoObrigarEnvioArquivo(){
+        given()
+            .log().all()
+        .when()
+            .post("http://restapi.wcaquino.me/upload")
+        .then()
+            .log().all()
+            .statusCode(404)
+            .body("error", is("Arquivo não enviado"))
+        ;
+    }
+}

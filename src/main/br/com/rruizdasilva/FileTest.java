470609e178962a4627d9bf41270648ddac988a6d
Roger <roger.dasilva@ul.com>
2019-12-04 08:45:11 -0300

    File upload

--- src/main/br/com/rruizdasilva/FileTest.java
+++ src/main/br/com/rruizdasilva/FileTest.java
@@ -3,6 +3,8 @@ package br.com.rruizdasilva;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
@@ -18,6 +20,20 @@ public class FileTest {
             .log().all()
             .statusCode(404)
             .body("error", is("Arquivo não enviado"))
+        ;
+    }
+
+    @Test
+    public void devoFazerUploadArquivo(){
+        given()
+        .log().all()
+        .multiPart("arquivo", new File("src/main/resources/users.pdf"))
+        .when()
+            .post("http://restapi.wcaquino.me/upload")
+        .then()
+        .log().all()
+        .statusCode(200)
+        .body("name", is("users.pdf"))
         ;
     }
 }
